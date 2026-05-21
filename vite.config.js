e8ad98b0e5414bda1e044d9fb85e8f3cb65aa8f1
Vanneck15 <vanneckgueb@gmail.com>
2026-05-21 08:39:42 -0400

    Fix: dynamic base path for local and production compatibility

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "/e-plantShopping/", // Indique à GitHub Pages de chercher dans le bon sous-dossier
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  return {
+    // Si on build (deploy), on utilise le sous-dossier GitHub, sinon en local on utilise la racine /
+    base: command === 'build' ? '/e-plantShopping/' : '/',
+    plugins: [react()],
+  }
 })
