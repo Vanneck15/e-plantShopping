834ca161dd65f18a40f1e9565eba574ed6ad811f
Vanneck15 <vanneckgueb@gmail.com>
2026-05-21 08:19:54 -0400

    Fix: update base URL to match repository name e-plantShopping

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Utilisation d'un chemin relatif pour éviter les erreurs 404
+  base: "/e-plantShopping/", // Indique à GitHub Pages de chercher dans le bon dépôt
   plugins: [react()],
 })
