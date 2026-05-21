f3a29843f0380e64b05c3bad49208ef420c42cc9
Vanneck15 <vanneckgueb@gmail.com>
2026-05-21 08:22:54 -0400

    Fix: set base path to /e-plantShopping/ to prevent GitHub Pages 404

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/e-plantShopping/", // Indique à GitHub Pages de chercher dans le bon dépôt
+  base: "/e-plantShopping/", // Indique à GitHub Pages de chercher dans le bon sous-dossier
   plugins: [react()],
 })
