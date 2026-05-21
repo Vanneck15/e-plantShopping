d5b305e7bc8265611b0897f537754b3b20e15c45
Vanneck15 <vanneckgueb@gmail.com>
2026-05-21 08:17:33 -0400

    Fix: change base path to relative to resolve 404 resource errors

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shoppingreact",
+  base: './', // Utilisation d'un chemin relatif pour éviter les erreurs 404
   plugins: [react()],
 })
